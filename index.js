e5e910a371906c026d8cd4235dbb8e503573ccfc
Eyosiyas <eyosiyas.tadesse@okstate.edu>
2018-08-20 08:32:53 -0500

    added uncaught exception handling using winston

--- index.js
+++ index.js
@@ -18,32 +18,36 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const app = express();
-
+/*
 winston.add(new winston.transports.File({ filename: 'logfile.log' }));
 //process object is an event emitter and raises a standard event called uncaughtException, this event is raised when we have an exception in the node process
 
 process.on('uncaughtException', (err) => {
     console.log('WE GOT AN UNCAUGHT EXCEPTION');
     winston.error(err.message, { message: err.message, stack: err.stack, timestamp: new Date().toString() }, null);
-    process.exit(1);
+    setTimeout(() => {        
+        process.exit(1);//it's best practice to exist the process if there is uncaught exception
+    }, 4000); //exit the process after 4 seconds which will give winston enough time to log the error
 }); 
 
 process.on('unhandledRejection', (err) => {
     console.log('WE GOT AN UNHANDLED REJECTION');
-    winston.error(err.message, 
-        { message: err.message, stack: err.stack, timestamp: new Date().toString() }, 
-        function(err, level, msg, meta) {
+    winston.error(err.message, { message: err.message, stack: err.stack, timestamp: new Date().toString() }, null);
+    setTimeout(() => {        
         process.exit(1);
-    });
+    }, 4000);
 }); 
-//winston.add(new winston.transports.File({filename: 'uncaughtExceptions.log', handleExceptions: true}));
-//winston.exceptions.handle(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
-/*
-process.on('unhandledRejection', (err) => {
-    throw err;
-});
 */
-//throw new Error('something failed during startup.');
+winston.exceptions.handle(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+winston.add(new winston.transports.File({
+    filename: 'uncaughtExceptions.log',
+    handleExceptions: true
+  }));
+process.on('unhandledRejection', (err) => {
+    throw err;//a workaround for winston exceptionHandler to handle unhandled Rejections
+});
+
+throw new Error('something failed during startup.');
 
 const p = Promise.reject(new Error('Something failed miserably!'));
 p.then(() => console.log('Done'));
